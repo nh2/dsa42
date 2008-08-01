ef24766a79fbfcc473a8dbe91e9a024cdec380de
fabio <fabio@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-01 15:41:15 +0000

    first program and first debugging exercise
    git-svn-id: http://deditus.de/svn/dsarepo42@46 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ teilnehmer/Fabio/Fibonacci/src/fibonacci/FibKaputt.java
@@ -0,0 +1,37 @@
+package fibonacci;
+
+public class FibKaputt
+{
+
+    private static final int CACHE_SIZE = 90;
+
+    private long[]           cache      = new long[CACHE_SIZE];
+
+    public FibKaputt()
+    {
+        cache[0] = 1;
+        cache[1] = 1;
+    }
+
+    public long compute(final int n)
+    {
+        if (cache[n - 1] > 0) return cache[n - 1];
+
+        long n2 = compute(n - 2);
+        long n1 = compute(n - 1);
+        long result = n1 + n2;
+        cache[n - 1] = result;
+        return result;
+    }
+
+    public static void main(final String[] args)
+    {
+        FibKaputt e = new FibKaputt();
+        for (int i = 1; i < 90; i++)
+        {
+            long result = e.compute(i);
+            System.out.println("e(" + i + ") = " + result);
+        }
+    }
+
+}
---
+++ teilnehmer/Fabio/Fibonacci/src/fibonacci/Fibonacci.java
@@ -0,0 +1,42 @@
+package fibonacci;
+
+public class Fibonacci
+{
+
+    public Fibonacci()
+    {
+        // TODO Auto-generated constructor stub
+    }
+
+    private int calculate(int k)
+    {
+        // TODO Auto-generated method stub
+        if (k == 1 || k == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return calculate(k - 1) + calculate(k - 2);
+        }
+
+    }
+
+    private void print(int n)
+    {
+        if (n < 0)
+        {
+            System.err.println("Keine negativen Zahlen!");
+        }
+        else
+        {
+            System.out.println(calculate(n));
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        Fibonacci fibonacci = new Fibonacci();
+        fibonacci.print(30);
+    }
+}
