40e445eb10a8fea17fb365b9d5e5636dee59e1c6
leonhard <leonhard@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-01 15:42:32 +0000

    Initial import
    git-svn-id: http://deditus.de/svn/dsarepo42@51 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ teilnehmer/leonhard/src/Fibonacci/FibKaputt.java
@@ -0,0 +1,36 @@
+package Fibonacci;
+
+public class FibKaputt
+{
+
+    private static final int CACHE_SIZE = 1000;
+
+    private long[]           cache      = new long[CACHE_SIZE];
+
+    public FibKaputt()
+    {
+        cache[0] = 1;
+        cache[1] = 1;
+    }
+
+    public long compute(final int n)
+    {
+        if (cache[n - 1] != 0) return cache[n - 1];
+        long n2 = compute(n - 2);
+        long n1 = compute(n - 1);
+        long result = n1 + n2;
+        cache[n - 1] = result;
+        return result;
+
+    }
+
+    public static void main(final String[] args)
+    {
+        FibKaputt e = new FibKaputt();
+        for (int i = 1; i < 900; i++)
+        {
+            long result = e.compute(i);
+            System.out.println("e(" + i + ") = " + result);
+        }
+    }
+}
