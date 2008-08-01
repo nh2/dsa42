e019574efc2c9c0f267f437b49ca7832396d3f34
sarah <sarah@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-01 15:52:54 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@76 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ teilnehmer/sarah/Fibonacci/src/Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci
+{
+
+    public static int fib(int number)
+    {
+        if (number == 0)
+        ;
+        if (number == 1 || number == 2)
+            return 1;
+
+        else
+        {
+            return fib(number - 1) + fib(number - 2);
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(fib(10));
+
+    }
+
+}
