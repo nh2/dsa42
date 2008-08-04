83b789dc3f6ffc360ce64d7f50de3677863d97b3
lars <lars@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-04 08:22:19 +0000

    created Getter for Card
    git-svn-id: http://deditus.de/svn/dsarepo42@144 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Card.java
+++ projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Card.java
@@ -3,12 +3,22 @@ package de.schelklingen2008.canasta.model;
 public class Card
 {
 
-    private Suit suit;
+    private Suit suit; // Jokers will save NULL
     private Rank rank;
 
     public Card(Rank rank, Suit suit)
     {
         this.rank = rank;
         this.suit = suit;
+    }
+
+    public Suit getSuit()
+    {
+        return suit;
+    }
+
+    public Rank getRank()
+    {
+        return rank;
     }
 }
