4ae791301066eebb37296f12ca72726bfa3da62a
lars <lars@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-04 09:58:14 +0000

    create class interfaces
    git-svn-id: http://deditus.de/svn/dsarepo42@165 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Hand.java
+++ projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Hand.java
@@ -1,7 +1,49 @@
 package de.schelklingen2008.canasta.model;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
-public class Hand
+public class Hand implements Iterable<Card>
 {
 
+    private List<Card> cards;
+
+    public Hand()
+    {
+        super();
+
+        cards = new LinkedList<Card>();
+    }
+
+    public boolean add(Card e)
+    {
+        return cards.add(e);
+    }
+
+    public Card get(int index)
+    {
+        return cards.get(index);
+    }
+
+    public Card remove(int index)
+    {
+        return cards.remove(index);
+    }
+
+    public int size()
+    {
+        return cards.size();
+    }
+
+    @Override
+    public String toString()
+    {
+        return cards.toString();
+    }
+
+    public Iterator<Card> iterator()
+    {
+        return cards.iterator();
+    }
 }
--- projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Outlay.java
+++ projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Outlay.java
@@ -1,6 +1,43 @@
 package de.schelklingen2008.canasta.model;
 
-public class Outlay
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Outlay implements Iterable<CardStack>
 {
+
+    private List<CardStack> cardStacks;
+
+    public Outlay()
+    {
+        super();
+        cardStacks = new ArrayList<CardStack>();
+    }
+
+    public boolean add(CardStack e)
+    {
+        return cardStacks.add(e);
+    }
+
+    public CardStack get(int index)
+    {
+        return cardStacks.get(index);
+    }
+
+    public boolean isEmpty()
+    {
+        return cardStacks.isEmpty();
+    }
+
+    public Iterator<CardStack> iterator()
+    {
+        return cardStacks.iterator();
+    }
+
+    public int size()
+    {
+        return cardStacks.size();
+    }
 
 }
