c24cf4b5175a7faa305aef4394819cfec74f368f
daniel <daniel@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-04 07:29:25 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@122 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ projekte/Billiards/src/main/java/de/schelklingen2008/billiards/client/controller/GameChangeListener.java
@@ -0,0 +1,6 @@
+package de.schelklingen2008.billiards.client.controller;
+
+public interface GameChangeListener
+{
+    void gameChanged();
+}
---
+++ projekte/Billiards/src/test/java/de/schelklingen2008/billiards/shared/GameModelTest.java
@@ -0,0 +1,14 @@
+package de.schelklingen2008.billiards.shared;
+
+import junit.framework.TestCase;
+import de.schelklingen2008.billiards.model.GameModel;
+
+public class GameModelTest extends TestCase
+{
+    private GameModel gameModel = new GameModel();
+
+    public void testSomething() throws Exception
+    {
+        // TODO add tests to test the rules and the state of the game
+    }
+}
