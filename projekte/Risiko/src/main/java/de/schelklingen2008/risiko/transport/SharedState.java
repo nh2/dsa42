1b1b5c601f1dfcce4d99f4c2237396b6de053396
leonhard <leonhard@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-04 08:07:36 +0000

    initial commit of Risiko
    git-svn-id: http://deditus.de/svn/dsarepo42@136 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ projekte/Risiko/src/main/java/de/schelklingen2008/risiko/transport/SharedState.java
@@ -0,0 +1,23 @@
+package de.schelklingen2008.risiko.transport;
+
+import com.threerings.parlor.game.data.GameObject;
+
+/**
+ * Keeps the shared state of the game for client-server communication and updates.
+ * 
+ * Note, that all fields must be public.
+ * 
+ * Note, that a call to "Generate Distributed Objects" is necessary after any change to this class.
+ */
+public class SharedState extends GameObject
+{
+    // TODO add fields describing the game state and start code generation afterewards
+
+    public String[] getPlayerNames()
+    {
+        String[] names = new String[players.length];
+        for (int i = 0; i < players.length; i++)
+            names[i] = players[i].toString();
+        return names;
+    }
+}
