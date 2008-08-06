350743514eed8d4b02a197d7032edd66873ddecf
niklasr <niklasr@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 16:51:09 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@310 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/PlayerCardsPanel.java
+++ projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/PlayerCardsPanel.java
@@ -1,8 +1,43 @@
 package de.schelklingen2008.dasverruecktelabyrinth.client.view;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JPanel;
+
+import de.schelklingen2008.dasverruecktelabyrinth.client.controller.Controller;
+import de.schelklingen2008.dasverruecktelabyrinth.client.model.GameContext;
+import de.schelklingen2008.dasverruecktelabyrinth.model.GameModel;
+import de.schelklingen2008.dasverruecktelabyrinth.model.PlayerCards;
+import de.schelklingen2008.dasverruecktelabyrinth.model.PlayerType;
+import de.schelklingen2008.dasverruecktelabyrinth.model.TreasureCard;
 
 public class PlayerCardsPanel extends JPanel
 {
-    public PlayerCardsPanel
 
+    private Controller controller;
+
+    public PlayerCardsPanel(Controller controller)
+    {
+        this.controller = controller;
+
+        Map<PlayerType, PlayerCards> MapWtf = getGameModel().getPlayerCardsMap();
+
+        PlayerCards karten = MapWtf.get(PlayerType.WHITE);
+        List<TreasureCard> open = karten.getOpenCards();
+        List<TreasureCard> hidden = karten.getHiddenCards();
+
+        // TODO auf listen zugreifen
+
+    }
+
+    private GameModel getGameModel()
+    {
+        return getGameContext().getGameModel();
+    }
+
+    private GameContext getGameContext()
+    {
+        return controller.getGameContext();
+    }
 }
