01e5d54e8db93dbd40bfb0fea5b587dfee02e832
niklasr <niklasr@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 15:02:31 +0000

    laby
    git-svn-id: http://deditus.de/svn/dsarepo42@246 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/LabyrinthPanel.java
+++ projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/LabyrinthPanel.java
@@ -2,12 +2,35 @@ package de.schelklingen2008.dasverruecktelabyrinth.client.view;
 
 import java.awt.BorderLayout;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
 public class LabyrinthPanel extends JPanel
 {
 
     public LabyrinthPanel(){
+        JButton pushButton1 = new JButton("hier");
+        JButton pushButton2 = new JButton("hier");
+        JButton pushButton3 = new JButton("hier");
+        JButton pushButton4 = new JButton("hier");
+        JButton pushButton5 = new JButton("hier");
+        JButton pushButton6 = new JButton("hier");
+        JButton pushButton7 = new JButton("hier");
+        JButton pushButton8 = new JButton("hier");
+        JButton pushButton9 = new JButton("hier");
+        JButton pushButton10 = new JButton("hier");
+        JButton pushButton11 = new JButton("hier");
+        JButton pushButton12 = new JButton("hier");
+
+        
+        JPanel buttonsSued = new JPanel();
+        buttons.setLayout(new BoxLayout(buttons, BoxLayout.PAGE_AXIS));
+        
+        
+        
+        
         setLayout(new BorderLayout());
         add()
     }
-
 }
