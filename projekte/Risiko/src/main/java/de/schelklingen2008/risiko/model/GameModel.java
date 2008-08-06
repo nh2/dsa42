d8680dc114098d5ff3756c3655a84eb3aeb2c5c1
lena <lena@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 15:46:41 +0000

    1
    git-svn-id: http://deditus.de/svn/dsarepo42@273 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/Risiko/src/main/java/de/schelklingen2008/risiko/model/GameModel.java
+++ projekte/Risiko/src/main/java/de/schelklingen2008/risiko/model/GameModel.java
@@ -6,43 +6,46 @@ package de.schelklingen2008.risiko.model;
 public class GameModel
 {
 
-    Country         island;
-    Country         irland;
-    Country         gb;
-    Country         norwegen;
-    Country         finnland;
-    Country         schweden;
-    Country         daenemark;
-    Country         russland;
-    Country         estland;
-    Country         lettland;
-    Country         litauen;
-    Country         weissrussland;
-    Country         ukraine;
-    Country         polen;
-    Country         italien;
-    Country         griechenland;
-    Country         jugoslawien;
-    Country         albanien;
-    Country         bulgarien;
-    Country         rumaenien;
-    Country         portugal;
-    Country         frankreich;
-    Country         spanien;
-    Country         benelux;
-    Country         deutschland;
-    Country         tschechien;
-    Country         schweiz;
-    Country         slowakei;
-    Country         oesterreich;
-    Country         ungarn;
+    private Country         island;
+    private Country         irland;
+    private Country         gb;
+    private Country         norwegen;
+    private Country         finnland;
+    private Country         schweden;
+    private Country         daenemark;
+    private Country         russland;
+    private Country         estland;
+    private Country         lettland;
+    private Country         litauen;
+    private Country         weissrussland;
+    private Country         ukraine;
+    private Country         polen;
+    private Country         italien;
+    private Country         griechenland;
+    private Country         jugoslawien;
+    private Country         albanien;
+    private Country         bulgarien;
+    private Country         rumaenien;
+    private Country         portugal;
+    private Country         frankreich;
+    private Country         spanien;
+    private Country         benelux;
+    private Country         deutschland;
+    private Country         tschechien;
+    private Country         schweiz;
+    private Country         slowakei;
+    private Country         oesterreich;
+    private Country         ungarn;
 
-    Country[]       c;
-    static Player[] p;
+    private Country[]       c;
+    private static Player[] p;
+    private Player          turnholder;
+    private int             player;
 
     public GameModel()
     {
         initCountrys();
+        player = 0;
 
     }
 
@@ -52,44 +55,59 @@ public class GameModel
         return null;
     }
 
+    public Country[] getCountryArray()
+    {
+        return c;
+    }
+
     public Country getCountry(int i)
     {
         return c[i];
     }
 
+    public Player getTurnholder()
+    {
+        return turnholder;
+    }
+
     private void initCountrys()
     {
-        island = new Country("Island", 0, 0, 0, null);
-        irland = new Country("Irland", 1, 0, 0, null);
-        gb = new Country("Groﬂbritanien", 2, 0, 0, null);
-        norwegen = new Country("Norwegen", 3, 0, 0, null);
-        finnland = new Country("Finnland", 4, 0, 0, null);
-        schweden = new Country("Schweden", 5, 0, 0, null);
-        daenemark = new Country("D‰nemark", 6, 0, 0, null);
-        russland = new Country("Russland", 7, 0, 0, null);
-        estland = new Country("Estland", 8, 0, 0, null);
-        lettland = new Country("Lettland", 9, 0, 0, null);
-        litauen = new Country("Litauen", 10, 0, 0, null);
-        weissrussland = new Country("Weiﬂrussland", 11, 0, 0, null);
-        ukraine = new Country("Ukraine", 12, 0, 0, null);
-        polen = new Country("Polen", 13, 0, 0, null);
-        italien = new Country("Italien", 14, 0, 0, null);
-        griechenland = new Country("Griechenland", 15, 0, 0, null);
-        jugoslawien = new Country("Jugoslawien", 16, 0, 0, null);
-        albanien = new Country("Albanien", 17, 0, 0, null);
-        bulgarien = new Country("Bulgarien", 18, 0, 0, null);
-        rumaenien = new Country("Rum‰nien", 19, 0, 0, null);
-        portugal = new Country("Portugal", 20, 0, 0, null);
-        frankreich = new Country("Frankreich", 21, 0, 0, null);
-        spanien = new Country("Spanien", 22, 0, 0, null);
-        benelux = new Country("BeNeLux", 23, 0, 0, null);
-        deutschland = new Country("Deutschland", 24, 0, 0, null);
-        tschechien = new Country("Tschechien", 25, 0, 0, null);
-        schweiz = new Country("Schweiz", 26, 0, 0, null);
-        slowakei = new Country("Slowakei", 27, 0, 0, null);
-        oesterreich = new Country("÷sterreich", 28, 0, 0, null);
-        ungarn = new Country("Ungarn", 29, 0, 0, null);
+        island = new Country("Island", 0, 0, 0, 0, 0, 0);
+        irland = new Country("Irland", 1, 0, 0, 0, 0, 0);
+        gb = new Country("Groﬂbritanien", 2, 0, 0, 0, 0, 0);
+        norwegen = new Country("Norwegen", 3, 0, 0, 0, 0, 0);
+        finnland = new Country("Finnland", 4, 0, 0, 0, 0, 0);
+        schweden = new Country("Schweden", 5, 0, 0, 0, 0, 0);
+        daenemark = new Country("D‰nemark", 6, 0, 0, 0, 0, 0);
+        russland = new Country("Russland", 7, 0, 0, 0, 0, 0);
+        estland = new Country("Estland", 8, 0, 0, 0, 0, 0);
+        lettland = new Country("Lettland", 9, 0, 0, 0, 0, 0);
+        litauen = new Country("Litauen", 10, 0, 0, 0, 0, 0);
+        weissrussland = new Country("Weiﬂrussland", 11, 0, 0, 0, 0, 0);
+        ukraine = new Country("Ukraine", 12, 0, 0, 0, 0, 0);
+        polen = new Country("Polen", 13, 0, 0, 0, 0, 0);
+        italien = new Country("Italien", 14, 0, 0, 0, 0, 0);
+        griechenland = new Country("Griechenland", 15, 0, 0, 0, 0, 0);
+        jugoslawien = new Country("Jugoslawien", 16, 0, 0, 0, 0, 0);
+        albanien = new Country("Albanien", 17, 0, 0, 0, 0, 0);
+        bulgarien = new Country("Bulgarien", 18, 0, 0, 0, 0, 0);
+        rumaenien = new Country("Rum‰nien", 19, 0, 0, 0, 0, 0);
+        portugal = new Country("Portugal", 20, 0, 0, 0, 0, 0);
+        frankreich = new Country("Frankreich", 21, 0, 0, 0, 0, 0);
+        spanien = new Country("Spanien", 22, 0, 0, 0, 0, 0);
+        benelux = new Country("BeNeLux", 23, 0, 0, 0, 0, 0);
+        deutschland = new Country("Deutschland", 24, 0, 0, 0, 0, 0);
+        tschechien = new Country("Tschechien", 25, 0, 0, 0, 0, 0);
+        schweiz = new Country("Schweiz", 26, 0, 0, 0, 0, 0);
+        slowakei = new Country("Slowakei", 27, 0, 0, 0, 0, 0);
+        oesterreich = new Country("÷sterreich", 28, 0, 0, 0, 0, 0);
+        ungarn = new Country("Ungarn", 29, 0, 0, 0, 0, 0);
 
+    }
+
+    public void setTurnholder()
+    {
+        turnholder = p[player++];
     }
 
 }
