9cc4b9e973916811f722d719c7524c150063ffe5
niklasr <niklasr@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 14:56:44 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@243 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/LabyrinthPanel.java
@@ -0,0 +1,13 @@
+package de.schelklingen2008.dasverruecktelabyrinth.client.view;
+
+import java.awt.BorderLayout;
+
+public class LabyrinthPanel extends JPanel
+{
+
+    public LabyrinthPanel(){
+        setLayout(new BorderLayout());
+        add()
+    }
+
+}
