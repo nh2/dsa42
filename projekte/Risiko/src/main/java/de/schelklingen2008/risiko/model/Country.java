797d096e0885965ac1a08ac560fc24e748596557
lena <lena@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 10:17:51 +0000

    change country
    git-svn-id: http://deditus.de/svn/dsarepo42@227 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/Risiko/src/main/java/de/schelklingen2008/risiko/model/Country.java
+++ projekte/Risiko/src/main/java/de/schelklingen2008/risiko/model/Country.java
@@ -9,8 +9,56 @@ public class Country
 
     private String        name;
     private int           units;
+    private int           index;
+    private int           positionNameX;
+    private int           positionNameY;
     private Player        occupier;
     private List<Country> neighbours = new ArrayList<Country>();
-    private Color         c;
+    private Color         color;
+    private boolean       selected;
 
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getUnits()
+    {
+        return units;
+    }
+
+    public int getIndex()
+    {
+        return index;
+    }
+
+    public int getPositionNameX()
+    {
+        return positionNameX;
+    }
+
+    public int getPositionNameY()
+    {
+        return positionNameY;
+    }
+
+    public Player getOccupier()
+    {
+        return occupier;
+    }
+
+    public boolean isNeighbour(Country c)
+    {
+        return neighbours.contains(c);
+    }
+
+    public Color getColor()
+    {
+        return color;
+    }
+
+    public boolean isSelected()
+    {
+        return selected;
+    }
 }
