f5fce860df27711af60993b015d4a2734cac7fce
sophie <sophie@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 15:47:45 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@276 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/PlayerPanel.java
+++ projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/PlayerPanel.java
@@ -1,5 +1,8 @@
 package de.schelklingen2008.dasverruecktelabyrinth.client.view;
 
+import java.awt.BorderLayout;
+import java.awt.Button;
+
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
@@ -8,9 +11,17 @@ public class PlayerPanel extends JPanel
 
     public PlayerPanel()
     {
+        setLayout(new BorderLayout());
+
         JPanel drehButtons = new JPanel();
 
         drehButtons.setLayout(new BoxLayout(drehButtons, BoxLayout.PAGE_AXIS));
+        drehButtons.add(new Button("rechts drehen"));
+        drehButtons.add(new Button("links drehen"));
+
+        JPanel insertTile = new JPanel();
+
+        insertTile.setLayout(new BorderLayout());
 
     }
 }
