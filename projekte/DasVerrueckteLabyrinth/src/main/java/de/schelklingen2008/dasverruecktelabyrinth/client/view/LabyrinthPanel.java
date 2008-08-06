abefae668cf38bbb7a14d3cb7a17877334e42154
niklasr <niklasr@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 15:14:56 +0000

    main methode zu labyPanel hinzugefügt
    git-svn-id: http://deditus.de/svn/dsarepo42@256 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/LabyrinthPanel.java
+++ projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/LabyrinthPanel.java
@@ -4,6 +4,7 @@ import java.awt.BorderLayout;
 
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 import javax.swing.JPanel;
 
 public class LabyrinthPanel extends JPanel
@@ -30,35 +31,38 @@ public class LabyrinthPanel extends JPanel
         add(pushButton2);
         add(pushButton3);
 
-
         JPanel buttonsOst = new JPanel();
         buttonsOst.setLayout(new BoxLayout(buttonsOst, BoxLayout.PAGE_AXIS));
         add(pushButton4);
         add(pushButton5);
         add(pushButton6);
 
-
-        
         JPanel buttonsNord = new JPanel();
         buttonsNord.setLayout(new BoxLayout(buttonsNord, BoxLayout.PAGE_AXIS));
         add(pushButton7);
         add(pushButton8);
         add(pushButton9);
 
-
-        
         JPanel buttonsWest = new JPanel();
         buttonsWest.setLayout(new BoxLayout(buttonsWest, BoxLayout.PAGE_AXIS));
         add(pushButton10);
         add(pushButton11);
         add(pushButton12);
 
-
-        
         setLayout(new BorderLayout());
         add(buttonsSued, BorderLayout.SOUTH);
         add(buttonsNord, BorderLayout.NORTH);
         add(buttonsWest, BorderLayout.WEST);
         add(buttonsOst, BorderLayout.EAST);
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame();
+        frame.getContentPane().add(new LabyrinthPanel());
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
