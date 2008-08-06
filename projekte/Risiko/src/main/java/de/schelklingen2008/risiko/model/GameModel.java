ff984287535f60e7106d624c1cbd566e8e3b6599
lena <lena@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-06 16:22:07 +0000

    koordinaten update #1
    git-svn-id: http://deditus.de/svn/dsarepo42@292 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/Risiko/src/main/java/de/schelklingen2008/risiko/model/GameModel.java
+++ projekte/Risiko/src/main/java/de/schelklingen2008/risiko/model/GameModel.java
@@ -6,7 +6,7 @@ package de.schelklingen2008.risiko.model;
 public class GameModel
 {
 
-    private Country[]       c;
+    private Country[]       c = new Country[30];
     private static Player[] p;
     private Player          turnholder;
     private int             player;
@@ -41,31 +41,31 @@ public class GameModel
 
     private void initCountrys()
     {
-        c[0] = new Country("Island", 0, 0, 0, 0, 0, 0);
+        c[0] = new Country("Island", 0, 90, 160, 0, 0, 0);
         c[1] = new Country("Irland", 1, 0, 0, 0, 0, 0);
         c[2] = new Country("Groﬂbritanien", 2, 0, 0, 0, 0, 0);
         c[3] = new Country("Norwegen", 3, 0, 0, 0, 0, 0);
         c[4] = new Country("Finnland", 4, 0, 0, 0, 0, 0);
         c[5] = new Country("Schweden", 5, 0, 0, 0, 0, 0);
         c[6] = new Country("D‰nemark", 6, 0, 0, 0, 0, 0);
-        c[7] = new Country("Russland", 7, 0, 0, 0, 0, 0);
+        c[7] = new Country("Russland", 7, 822, 336, 0, 0, 0);
         c[8] = new Country("Estland", 8, 0, 0, 0, 0, 0);
         c[9] = new Country("Lettland", 9, 0, 0, 0, 0, 0);
         c[10] = new Country("Litauen", 10, 0, 0, 0, 0, 0);
         c[11] = new Country("Weiﬂrussland", 11, 0, 0, 0, 0, 0);
         c[12] = new Country("Ukraine", 12, 0, 0, 0, 0, 0);
-        c[13] = new Country("Polen", 13, 0, 0, 0, 0, 0);
-        c[14] = new Country("Italien", 14, 0, 0, 0, 0, 0);
-        c[15] = new Country("Griechenland", 15, 0, 0, 0, 0, 0);
+        c[13] = new Country("Polen", 13, 527, 528, 0, 0, 0);
+        c[14] = new Country("Italien", 14, 400, 724, 0, 0, 0);
+        c[15] = new Country("Griechenland", 15, 652, 848, 0, 0, 0);
         c[16] = new Country("Jugoslawien", 16, 0, 0, 0, 0, 0);
         c[17] = new Country("Albanien", 17, 0, 0, 0, 0, 0);
-        c[18] = new Country("Bulgarien", 18, 0, 0, 0, 0, 0);
+        c[18] = new Country("Bulgarien", 18, 652, 672, 0, 0, 0);
         c[19] = new Country("Rum‰nien", 19, 0, 0, 0, 0, 0);
-        c[20] = new Country("Portugal", 20, 0, 0, 0, 0, 0);
-        c[21] = new Country("Frankreich", 21, 0, 0, 0, 0, 0);
-        c[22] = new Country("Spanien", 22, 0, 0, 0, 0, 0);
-        c[23] = new Country("BeNeLux", 23, 0, 0, 0, 0, 0);
-        c[24] = new Country("Deutschland", 24, 0, 0, 0, 0, 0);
+        c[20] = new Country("Portugal", 20, 25, 803, 0, 0, 0);
+        c[21] = new Country("Frankreich", 21, 247, 674, 0, 0, 0);
+        c[22] = new Country("Spanien", 22, 113, 826, 0, 0, 0);
+        c[23] = new Country("BeNeLux", 23, 313, 549, 0, 0, 0);
+        c[24] = new Country("Deutschland", 24, 380, 564, 0, 0, 0);
         c[25] = new Country("Tschechien", 25, 0, 0, 0, 0, 0);
         c[26] = new Country("Schweiz", 26, 0, 0, 0, 0, 0);
         c[27] = new Country("Slowakei", 27, 0, 0, 0, 0, 0);
