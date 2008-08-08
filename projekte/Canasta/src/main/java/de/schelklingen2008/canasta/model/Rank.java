e2b5715aaeb7adffa5af81d4fabea3c212c74b0c
lars <lars@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-08 16:49:47 +0000

    changed rank order (two now before joker)
    git-svn-id: http://deditus.de/svn/dsarepo42@484 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Rank.java
+++ projekte/Canasta/src/main/java/de/schelklingen2008/canasta/model/Rank.java
@@ -2,5 +2,5 @@ package de.schelklingen2008.canasta.model;
 
 public enum Rank
 {
-    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE, JOKER;
+    THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE, TWO, JOKER;
 }
