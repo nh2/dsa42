50cc769a69ff5fe0f1059408572a750f39166e2e
niklasr <niklasr@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-11 15:08:57 +0000

    TurnPanel mit TurnAnzeige und richtigen Farben
    git-svn-id: http://deditus.de/svn/dsarepo42@697 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/Constants.java
+++ projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/Constants.java
@@ -1,6 +1,10 @@
 package de.schelklingen2008.dasverruecktelabyrinth.client;
 
 import java.awt.Color;
+import java.util.HashMap;
+import java.util.Map;
+
+import de.schelklingen2008.dasverruecktelabyrinth.model.PlayerType;
 
 /**
  * Keeps all constant values that are of global interest for the client.
@@ -9,15 +13,26 @@ public final class Constants
 {
 
     /** must correspond to the name of the message properties file */
-    public static final String BUNDLE_NAME          = "DasVerrueckteLabyrinth";
+    public static final String                 BUNDLE_NAME          = "DasVerrueckteLabyrinth";
 
     /** names of message properties */
-    public static final String MSG_BACK_TO_LOBBY    = "m.back_to_lobby";
-    public static final String MSG_TITLE            = "m.title";
-    public static final String MSG_WINNER           = "m.winner";
-    public static final String MSG_DRAW             = "m.draw";
+    public static final String                 MSG_BACK_TO_LOBBY    = "m.back_to_lobby";
+    public static final String                 MSG_TITLE            = "m.title";
+    public static final String                 MSG_WINNER           = "m.winner";
+    public static final String                 MSG_DRAW             = "m.draw";
 
     /** is the pixel dimension of the pieces */
-    public static final int    SPRITE_SIZE          = 80;
-    public static final Color  COL_BOARD_BACKGROUND = new Color(0, 0, 0);
+    public static final int                    SPRITE_SIZE          = 80;
+    public static final Color                  COL_BOARD_BACKGROUND = new Color(0, 0, 0);
+
+    /** piece colors and insets */
+    public static final Map<PlayerType, Color> COL_PIECE            = new HashMap<PlayerType, Color>();
+    static
+    {
+        COL_PIECE.put(PlayerType.WHITE, Color.YELLOW);
+        COL_PIECE.put(PlayerType.BLACK, Color.BLUE);
+        COL_PIECE.put(PlayerType.RED, Color.RED);
+        COL_PIECE.put(PlayerType.GREEN, Color.GREEN);
+
+    }
 }
--- projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/TurnPanel.java
+++ projekte/DasVerrueckteLabyrinth/src/main/java/de/schelklingen2008/dasverruecktelabyrinth/client/view/TurnPanel.java
@@ -1,9 +1,17 @@
 package de.schelklingen2008.dasverruecktelabyrinth.client.view;
 
-import javax.swing.BoxLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Polygon;
+import java.awt.geom.Ellipse2D;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import com.samskivert.swing.ShapeIcon;
+
+import de.schelklingen2008.dasverruecktelabyrinth.client.Constants;
 import de.schelklingen2008.dasverruecktelabyrinth.client.controller.Controller;
 import de.schelklingen2008.dasverruecktelabyrinth.client.controller.GameChangeListener;
 import de.schelklingen2008.dasverruecktelabyrinth.client.model.GameContext;
@@ -16,12 +24,17 @@ import de.schelklingen2008.dasverruecktelabyrinth.model.PlayerType;
 public class TurnPanel extends JPanel implements GameChangeListener
 {
 
-    private Controller controller;
+    private Controller             controller;
+    private static final Polygon   TRIANGLE  = new Polygon(new int[] { 0, 12, 0 }, new int[] { 0, 6, 12 }, 3);
+    private static final Ellipse2D CIRCLE    = new Ellipse2D.Float(0, 0, 12, 12);
+    private static final ShapeIcon ICON_TURN = new ShapeIcon(TRIANGLE, Color.YELLOW, null);
 
     public TurnPanel(Controller controller)
     {
         this.controller = controller;
         controller.addChangeListener(this);
+
+        gameChanged();
     }
 
     public void gameChanged()
@@ -29,12 +42,34 @@ public class TurnPanel extends JPanel implements GameChangeListener
         removeAll();
         if (getGameModel() == null) return;
 
-        setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        GridBagLayout gridbag = new GridBagLayout();
+        setLayout(gridbag);
+
+        GridBagConstraints turnHolderConstraints = new GridBagConstraints();
+        turnHolderConstraints.fill = GridBagConstraints.BOTH;
+
+        GridBagConstraints namesConstraints = new GridBagConstraints();
+        namesConstraints.fill = GridBagConstraints.BOTH;
+        namesConstraints.weightx = 1.0;
+        namesConstraints.insets.left = 10;
+        namesConstraints.gridwidth = GridBagConstraints.REMAINDER;
+
+        PlayerType turnHolder = getGameModel().getTurnHolder();
 
         for (PlayerType playerType : PlayerType.values())
         {
+            JLabel turnHolderLabel = new JLabel();
+            turnHolderLabel.setForeground(Color.BLACK);
+            if (playerType.equals(turnHolder)) turnHolderLabel.setIcon(ICON_TURN);
+            add(turnHolderLabel, turnHolderConstraints);
+
             String name = getGameModel().getName(playerType);
-            add(new JLabel(name));
+
+            Color color = Constants.COL_PIECE.get(playerType);
+
+            JLabel namesLabel = new JLabel(name);
+            namesLabel.setIcon(new ShapeIcon(CIRCLE, color, null));
+            add(namesLabel, namesConstraints);
         }
 
         revalidate();
