fc43ba6b847798873ea895d870cde7f7e06ef929
maja <maja@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-11 16:35:54 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@734 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/MMPoker/src/main/java/de/schelklingen2008/mmpoker/client/view/BoardView.java
+++ projekte/MMPoker/src/main/java/de/schelklingen2008/mmpoker/client/view/BoardView.java
@@ -24,8 +24,7 @@ import de.schelklingen2008.mmpoker.model.Spielstadien;
 /**
  * Displays the main game interface (the board).
  */
-public class BoardView extends JPanel implements GameChangeListener
-{
+public class BoardView extends JPanel implements GameChangeListener {
 
     private Controller controller;
 
@@ -34,12 +33,9 @@ public class BoardView extends JPanel implements GameChangeListener
     /**
      * Constructs a view which will initialize itself and prepare to display the game board.
      */
-    public BoardView(Controller controller)
-    {
-        for (Kartentyp typ : Kartentyp.values())
-        {
-            for (Kartenwert wert : Kartenwert.values())
-            {
+    public BoardView(Controller controller) {
+        for (Kartentyp typ : Kartentyp.values()) {
+            for (Kartenwert wert : Kartenwert.values()) {
                 loadImage(typ, wert);
             }
         }
@@ -49,28 +45,23 @@ public class BoardView extends JPanel implements GameChangeListener
 
     }
 
-    private void loadImage(Kartentyp typ, Kartenwert wert)
-    {
+    private void loadImage(Kartentyp typ, Kartenwert wert) {
         String filename = "75/" + typ.getImageName() + "-" + wert.getImageName() + "-75.png";
 
-        try
-        {
+        try {
             BufferedImage image = ImageIO.read(new File(filename));
             cardImages[typ.ordinal()][wert.ordinal()] = image;
         }
-        catch (IOException e)
-        {
+        catch (IOException e) {
             throw new RuntimeException("Kann Bild " + filename + " nicht laden.");
         }
     }
 
-    public void gemeinschaftsKarten(Graphics g)
-    {
+    public void gemeinschaftsKarten(Graphics g) {
         Graphics2D gfx = (Graphics2D) g;
         if (getGameModel().getSpielstadium() == Spielstadien.FLOP
             || getGameModel().getSpielstadium() == Spielstadien.TURN
-            || getGameModel().getSpielstadium() == Spielstadien.RIVER)
-        {
+            || getGameModel().getSpielstadium() == Spielstadien.RIVER) {
             gfx.drawImage(
                           cardImages[getGameModel().getSpielfeld()[1].getKartentyp().ordinal()][getGameModel().getSpielfeld()[1].getKartenwert()
                                                                                                                                 .ordinal()],
@@ -84,14 +75,12 @@ public class BoardView extends JPanel implements GameChangeListener
                                                                                                                                 .ordinal()],
                           385, 350, null);
             if (getGameModel().getSpielstadium() == Spielstadien.TURN
-                || getGameModel().getSpielstadium() == Spielstadien.RIVER)
-            {
+                || getGameModel().getSpielstadium() == Spielstadien.RIVER) {
                 gfx.drawImage(
                               cardImages[getGameModel().getSpielfeld()[3].getKartentyp().ordinal()][getGameModel().getSpielfeld()[3].getKartenwert()
                                                                                                                                     .ordinal()],
                               465, 350, null);
-                if (getGameModel().getSpielstadium() == Spielstadien.RIVER)
-                {
+                if (getGameModel().getSpielstadium() == Spielstadien.RIVER) {
                     gfx.drawImage(
                                   cardImages[getGameModel().getSpielfeld()[3].getKartentyp().ordinal()][getGameModel().getSpielfeld()[3].getKartenwert()
                                                                                                                                         .ordinal()],
@@ -102,29 +91,25 @@ public class BoardView extends JPanel implements GameChangeListener
 
     }
 
-    public void handKarten(Graphics g)
-    {
+    public void handKarten(Graphics g) {
         Graphics2D gfx = (Graphics2D) g;
         g.setColor(Color.black);
-        if (!(getGameModel().getAmZug().getLetzteAktion() == LetzteAktion.FOLD))
-        {
+        if (!(getGameModel().getAmZug().getLetzteAktion() == LetzteAktion.FOLD)) {
             // zeichne
             Spielkarte[] handblatt = getGameContext().getMyPlayer().getHandblatt();
             gfx.drawImage(cardImages[handblatt[0].getKartentyp().ordinal()][handblatt[0].getKartenwert().ordinal()],
-                          305, 600, null);
+                          305, 650, null);
             gfx.drawImage(cardImages[handblatt[1].getKartentyp().ordinal()][handblatt[1].getKartenwert().ordinal()],
-                          385, 600, null);
+                          385, 650, null);
         }
-        else
-        {
+        else {
 
         }
 
     }
 
     @Override
-    public void paintComponent(Graphics g)
-    {
+    public void paintComponent(Graphics g) {
         Graphics2D gfx = (Graphics2D) g;
         g.setColor(Color.GREEN);
         g.fillRect(0, 0, 800, 800);
@@ -135,30 +120,33 @@ public class BoardView extends JPanel implements GameChangeListener
         g.setColor(Color.BLACK);
 
         String pot = "$" + getGameModel().getPot();
-        gfx.drawString(pot, 200, 250);
+        gfx.drawString(pot, 200, 380);
 
         String anzeigeName = getGameContext().getMyName();
-        gfx.drawString(anzeigeName, 10, 10);
+        gfx.drawString(anzeigeName, 200, 700);
+
+        String geld = "$" + getGameContext().getMyPlayer().getGeld();
+        gfx.drawString(geld, 200, 725);
+
+        if (getGameModel().rundeBeendet()) {
+            getGameModel().setSpielstadium(getGameModel().getSpielstadium().next());
+        }
     }
 
     @Override
-    public Dimension getPreferredSize()
-    {
+    public Dimension getPreferredSize() {
         return new Dimension(800, 800);
     }
 
-    public void gameChanged()
-    {
+    public void gameChanged() {
         repaint();
     }
 
-    private GameModel getGameModel()
-    {
+    private GameModel getGameModel() {
         return getGameContext().getGameModel();
     }
 
-    private GameContext getGameContext()
-    {
+    private GameContext getGameContext() {
         return controller.getGameContext();
     }
 }
--- projekte/MMPoker/src/main/java/de/schelklingen2008/mmpoker/client/view/MalPanel.java
+++ projekte/MMPoker/src/main/java/de/schelklingen2008/mmpoker/client/view/MalPanel.java
@@ -1,51 +1,51 @@
-package de.schelklingen2008.mmpoker.client.view;
-
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
-
-public class MalPanel extends JPanel {
-
-    private BufferedImage karte;
-
-    public MalPanel() {
-        try {
-            karte = ImageIO.read(new File("Karten/clubs-8-150.png"));
-        }
-        catch (IOException e) {
-            throw new RuntimeException("Kann Bild nicht laden.");
-        }
-
-    }
-
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(500, 500);
-    }
-
-    @Override
-    public void paint(Graphics g) {
-        Graphics2D gfx = (Graphics2D) g;
-        g.setColor(Color.GREEN);
-        // g.fillOval(50, 50, 10, 10);
-
-        for (int i = 1; i < 11; i++) {
-            gfx.drawImage(karte, i * 20, i * 20, 37, 53, null);
-        }
-    }
-
-    public static void main(String[] args) {
-        // JFrame frame = new JFrame();
-        // frame.getContentPane().add(new MalPanel());
-        // frame.pack();
-        // frame.setVisible(true);
-    }
-
-}
+//package de.schelklingen2008.mmpoker.client.view;
+//
+//import java.awt.Color;
+//import java.awt.Dimension;
+//import java.awt.Graphics;
+//import java.awt.Graphics2D;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
+//import java.io.IOException;
+//
+//import javax.imageio.ImageIO;
+//import javax.swing.JPanel;
+//
+//public class MalPanel extends JPanel {
+//
+//    private BufferedImage karte;
+//
+//    public MalPanel() {
+//        try {
+//            karte = ImageIO.read(new File("Karten/clubs-8-150.png"));
+//        }
+//        catch (IOException e) {
+//            throw new RuntimeException("Kann Bild nicht laden.");
+//        }
+//
+//    }
+//
+//    @Override
+//    public Dimension getPreferredSize() {
+//        return new Dimension(500, 500);
+//    }
+//
+//    @Override
+//    public void paint(Graphics g) {
+//        Graphics2D gfx = (Graphics2D) g;
+//        g.setColor(Color.GREEN);
+//        // g.fillOval(50, 50, 10, 10);
+//
+//        for (int i = 1; i < 11; i++) {
+//            gfx.drawImage(karte, i * 20, i * 20, 37, 53, null);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        // JFrame frame = new JFrame();
+//        // frame.getContentPane().add(new MalPanel());
+//        // frame.pack();
+//        // frame.setVisible(true);
+//    }
+//
+//}
