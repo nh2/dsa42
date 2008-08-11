1a71923661b67b3152b1dc7d59cd434a29a5ad10
elitsa <elitsa@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-11 07:12:16 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@588 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/JipBo/src/test/java/de/schelklingen2008/jipbo/shared/GameModelTest.java
+++ projekte/JipBo/src/test/java/de/schelklingen2008/jipbo/shared/GameModelTest.java
@@ -5,10 +5,24 @@ import de.schelklingen2008.jipbo.model.GameModel;
 
 public class GameModelTest extends TestCase
 {
+
     private GameModel gameModel = new GameModel();
 
-    public void testSomething() throws Exception
+    public void testCreateGame() throws Exception
     {
-        // TODO add tests to test the rules and the state of the game
+        String[] playerNames = new String[] { "Elitsa", "Fabio" };
+        gameModel.createGame(playerNames);
+        assertEquals("Should be Elitsa", "Elitsa", gameModel.getPlayerNameIndexOf(0));
+        assertEquals("Should be Fabio", "Fabio", gameModel.getPlayerNameIndexOf(1));
+
+        assertEquals("There should be 2 players", 2, gameModel.getPlayerSize());
+
+        assertEquals("This should be 0", 0, gameModel.getPlayerIDByName(new String("Elitsa")));
+        assertEquals("This should be 1", 1, gameModel.getPlayerIDByName(new String("Fabio")));
+        // assertEquals("This should be 2", -1, gameModel.getPlayerIDByName(new
+        // String("Kein Spieler gefunden.")));
+
+        assertEquals("This should be 3 ", 3, gameModel.getLastStockCard());
     }
+
 }
