0c7adb39e9fdf8dfd847f9c6d3213e45a0852bc4
moritz <moritz@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 08:42:55 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@755 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/AmorEvaluationFunction.java
@@ -0,0 +1,15 @@
+package de.schelklingen2008.reversi.ai.evaluation;
+
+import de.schelklingen2008.reversi.model.GameModel;
+import de.schelklingen2008.reversi.model.Player;
+
+public class AmorEvaluationFunction implements EvaluationFunction
+{
+
+    public int evaluatePosition(GameModel gameModel, Player player)
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+}
---
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/AmorStrategy.java
@@ -0,0 +1,21 @@
+package de.schelklingen2008.reversi.ai.strategy;
+
+import de.schelklingen2008.reversi.model.GameModel;
+import de.schelklingen2008.reversi.model.Piece;
+
+public class AmorStrategy implements ReversiStrategy
+{
+
+    public int getCount()
+    {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    public Piece move(GameModel gameModel)
+    {
+
+        return null;
+    }
+
+}
