e0dcbaeb3162f9defbd26daab8681ac645dcb678
fabio <fabio@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 10:06:40 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@776 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/tournament/MainFrame.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/tournament/MainFrame.java
@@ -2,9 +2,6 @@ package de.schelklingen2008.reversi.ai.tournament;
 
 import javax.swing.JFrame;
 
-import de.schelklingen2008.reversi.ai.evaluation.CornerEvaluationFunction;
-import de.schelklingen2008.reversi.ai.strategy.AlphaBetaStrategy;
-import de.schelklingen2008.reversi.ai.strategy.MinimaxStrategy;
 import de.schelklingen2008.reversi.ai.strategy.SimpleStrategy;
 
 public class MainFrame
@@ -14,10 +11,14 @@ public class MainFrame
     {
         Tournament t = new Tournament();
         t.addStrategy(new TournamentStrategy("Ben", new SimpleStrategy()));
-        t.addStrategy(new TournamentStrategy("Georg", new AlphaBetaStrategy(new CornerEvaluationFunction(), 1, true)));
-        t.addStrategy(new TournamentStrategy("Daniel", new AlphaBetaStrategy(new CornerEvaluationFunction(), 1, true)));
-        t.addStrategy(new TournamentStrategy("Manuel", new AlphaBetaStrategy(new CornerEvaluationFunction(), 1, true)));
-        t.addStrategy(new TournamentStrategy("Sarah", new MinimaxStrategy(new CornerEvaluationFunction(), 1, true)));
+        // t.addStrategy(new TournamentStrategy("Georg", new AlphaBetaStrategy(new CornerEvaluationFunction(),
+        // 1, true)));
+        // t.addStrategy(new TournamentStrategy("Daniel", new AlphaBetaStrategy(new
+        // CornerEvaluationFunction(), 1, true)));
+        // t.addStrategy(new TournamentStrategy("Manuel", new AlphaBetaStrategy(new
+        // CornerEvaluationFunction(), 1, true)));
+        // t.addStrategy(new TournamentStrategy("Sarah", new MinimaxStrategy(new CornerEvaluationFunction(),
+        // 1, true)));
         t.prepare();
 
         final JFrame frame = new JFrame();
