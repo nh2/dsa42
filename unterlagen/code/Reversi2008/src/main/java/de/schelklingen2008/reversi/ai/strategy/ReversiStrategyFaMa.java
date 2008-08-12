4e2484f5b237a0a8b024708bc4c49d34b0fce73c
fabio <fabio@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 08:18:59 +0000

    interfaces
    git-svn-id: http://deditus.de/svn/dsarepo42@748 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
@@ -1,7 +1,14 @@
 package de.schelklingen2008.reversi.ai.strategy;
 
+import de.schelklingen2008.reversi.model.GameModel;
+import de.schelklingen2008.reversi.model.Piece;
 
-public class ReversiStrategyFaMa
+public interface ReversiStrategyFaMa
 {
 
+    /** Requests a move from a strategy. */
+    Piece move(GameModel gameModel);
+
+    /** How many positions has the strategy considered before making the move? */
+    int getCount();
 }
