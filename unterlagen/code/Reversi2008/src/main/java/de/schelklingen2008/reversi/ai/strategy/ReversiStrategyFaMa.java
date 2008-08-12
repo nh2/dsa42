055b46126b30c068e5ee779aa5a2b99ac6d30c57
fabio <fabio@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 16:38:28 +0000

    fixed
    git-svn-id: http://deditus.de/svn/dsarepo42@799 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
@@ -1,16 +1,19 @@
 package de.schelklingen2008.reversi.ai.strategy;
 
 import java.util.Iterator;
+import java.util.Set;
 
 import de.schelklingen2008.reversi.ai.evaluation.EvaluationFunctionFaMa;
 import de.schelklingen2008.reversi.model.GameModel;
 import de.schelklingen2008.reversi.model.Piece;
+import de.schelklingen2008.reversi.model.Player;
 
 public class ReversiStrategyFaMa implements ReversiStrategy
 {
 
     private final EvaluationFunctionFaMa mEvalFunction;
-    private final int                    mDepth;
+    private int                          mDepth;
+    private Player                       mMe;
 
     public ReversiStrategyFaMa(EvaluationFunctionFaMa pEvalFunction, int pDepth)
     {
@@ -18,56 +21,50 @@ public class ReversiStrategyFaMa implements ReversiStrategy
         mDepth = pDepth;
     }
 
-    public Piece move(GameModel gameModel)
+    public Piece move(final GameModel gameModel)
     {
+        mMe = gameModel.getTurnHolder();
+        if (!gameModel.hasLegalMoves()) return null;
+
         int best = Integer.MIN_VALUE;
         Piece bestMove = null;
-        for (Iterator<Piece> iterator = gameModel.getLegalMovesSet(gameModel.getTurnHolder()).iterator(); iterator.hasNext();)
+
+        Set<Piece> legalMoves = gameModel.getLegalMovesSet(gameModel.getTurnHolder());
+        for (Iterator<Piece> iterator = legalMoves.iterator(); iterator.hasNext();)
         {
             Piece move = iterator.next();
             GameModel clone = new GameModel(gameModel); // erzeugt ein Klon des aktuellen Spielstands
             clone.placePiece(move);
-            int value = mmvalue(clone, mDepth, false);
+            int value = mmvalue(clone, mDepth, true);
+            // System.out.println("(" + move.getX() + "," + move.getY() + ") = " + value);
             if (value > best)
             {
                 best = value;
                 bestMove = move;
             }
         }
-        if (bestMove == null) throw new IllegalArgumentException("bestMove is null");
         return bestMove;
     }
 
-    public int mmvalue(GameModel gameModel, int depth, boolean isMax)
+    public int mmvalue(final GameModel gameModel, final int depth, final boolean isMax)
     {
         if (depth == 0 || !gameModel.hasLegalMoves())
         {
-            return mEvalFunction.evaluatePosition(gameModel, gameModel.getTurnHolder());
-        }
-        int best = 0;
-        if (isMax)
-        {
-            best = Integer.MIN_VALUE;
-        }
-        else
-        {
-            best = Integer.MAX_VALUE;
+            return mEvalFunction.evaluatePosition(gameModel, mMe);
         }
 
-        for (Iterator<Piece> iterator = gameModel.getLegalMovesSet(gameModel.getTurnHolder()).iterator(); iterator.hasNext();)
+        int best = isMax ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+
+        Set<Piece> legalMoves = gameModel.getLegalMovesSet(gameModel.getTurnHolder());
+        for (Iterator<Piece> iterator = legalMoves.iterator(); iterator.hasNext();)
         {
             Piece move = iterator.next();
+
             GameModel clone = new GameModel(gameModel); // erzeugt ein Klon des aktuellen Spielstands
             clone.placePiece(move);
-            int value = mmvalue(clone, depth - 1, (isMax ? false : true));
-            if (isMax)
-            {
-                if (best > value) best = value;
-            }
-            else
-            {
-                if (best < value) best = value;
-            }
+            int value = mmvalue(clone, depth - 1, !isMax);
+
+            if (isMax && value > best || !isMax && value < best) best = value;
         }
 
         return best;
