4835254175c58b60c0bbb1e3e546827d7f2d020d
matthias <matthias@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 09:52:43 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@770 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
@@ -1,5 +1,7 @@
 package de.schelklingen2008.reversi.ai.strategy;
 
+import java.util.Iterator;
+
 import de.schelklingen2008.reversi.model.GameModel;
 import de.schelklingen2008.reversi.model.Piece;
 
@@ -10,18 +12,19 @@ public class ReversiStrategyFaMa implements ReversiStrategy
     {
         int best = Integer.MIN_VALUE;
         Piece bestMove = null;
-        for (int i = 0; i < GameModel.SIZE; i++)
+        for (Iterator iterator = gameModel.getLegalMovesSet(gameModel.getTurnHolder()).iterator(); iterator.hasNext();)
         {
-            for (int j = 0; j < GameModel.SIZE; j++)
-            {
-                if (gameModel.getLegalMoves(gameModel.getTurnHolder())[i][j])
-                {
-                    GameModel clone = new GameModel(gameModel); // erzeugt ein Klon des aktuellen Spielstands
-                    clone.placePiece(i, j, gameModel.getTurnHolder());
-                    int value = mmvalue(clone, 5);
+            Piece move = (Piece) iterator.next();
+            GameModel clone = new GameModel(gameModel); // erzeugt ein Klon des aktuellen Spielstands
+            clone.placePiece(move.getX(),move.getY(),move.getPlayer());
+            int value = mmvalue(clone, 5);
+                    if (value > best)
+                    {
+                        best = value;
+                        bestMove = move;
+                    }
                 }
-            }
-        }
+ }
         return null;
     }
 
