b2a77fdab58ddb84b06283d5f07024927469cae7
fabio <fabio@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 08:25:12 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@751 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/EvaluationFunctionFaMa.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/EvaluationFunctionFaMa.java
@@ -1,7 +1,13 @@
 package de.schelklingen2008.reversi.ai.evaluation;
 
+import de.schelklingen2008.reversi.model.GameModel;
+import de.schelklingen2008.reversi.model.Player;
 
-public class EvaluationFunctionFaMa
+public class EvaluationFunctionFaMa implements EvaluationFunction
 {
 
+    public int evaluatePosition(GameModel gameModel, Player player)
+    {
+        return 0;
+    }
 }
