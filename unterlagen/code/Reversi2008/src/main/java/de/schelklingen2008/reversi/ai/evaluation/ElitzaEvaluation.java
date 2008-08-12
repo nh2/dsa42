ab43c7df2e80c3de8a7525e3cdca4072394d0df6
elitsa <elitsa@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 16:41:33 +0000

    ElitzaEvaluation
    git-svn-id: http://deditus.de/svn/dsarepo42@802 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/ElitzaEvaluation.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/ElitzaEvaluation.java
@@ -4,29 +4,10 @@ import de.schelklingen2008.reversi.model.GameModel;
 import de.schelklingen2008.reversi.model.Player;
 
 public class ElitzaEvaluation implements EvaluationFunction
-
 {
 
     public int evaluatePosition(GameModel gameModel, Player player)
     {
-        return 0;
+        return gameModel.countPieces(player);
     }
-
-    // int mnval(Game game, int depth ){
-    // if (depth = 0) {
-    // return eval(game);
-    // int best;
-    // int MAX;
-    // if (MAX = 0 ){
-    // best = -INF}
-    // else best = INF;
-    //                
-    // 
-    //                
-    // }
-    //
-    // private int eval(Game game)
-    // {
-    // return 0;
-    // }
 }
