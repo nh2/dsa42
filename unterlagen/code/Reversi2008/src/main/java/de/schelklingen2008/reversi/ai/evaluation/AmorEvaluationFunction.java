56d1ee9c9893cc1bbdc52256481a290e780c4882
moritz <moritz@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 09:22:31 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@758 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/AmorEvaluationFunction.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/AmorEvaluationFunction.java
@@ -8,8 +8,34 @@ public class AmorEvaluationFunction implements EvaluationFunction
 
     public int evaluatePosition(GameModel gameModel, Player player)
     {
-        // TODO Adwuto-generated method stub
+        int x = 0;
+        x = 64 - gameModel.countPieces(player);
+
         return 0;
     }
 
+    private void spielSteinBewertung(GameModel gameModel, Player player)
+    {
+        int[][] spielfeldbewertung = { { 800, 200, 200, 200, 200, 200, 200, 800 },
+                                       { 200, 50, 200, 200, 200, 200, 50, 200 },
+                                       { 200, 200, 200, 200, 200, 200, 200, 200 },
+                                       { 200, 200, 200, 200, 200, 200, 200, 200 },
+                                       { 200, 200, 200, 200, 200, 200, 200, 200 },
+                                       { 200, 200, 200, 200, 200, 200, 200, 200 },
+                                       { 200, 50, 200, 200, 200, 200, 50, 200 },
+                                       { 800, 200, 200, 200, 200, 200, 200, 800 }, };
+
+        int playerSteinBewertung = 0;
+        for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (gameModel.getBoard()[i][j] == player)
+                {
+                    playerSteinBewertung = playerSteinBewertung + spielfeldbewertung[i][j];
+                }
+            }
+
+        }
+    }
 }
