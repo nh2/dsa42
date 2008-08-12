0181eae3fd3f670bd279a81dfb33429bb80f5075
matthias <matthias@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 09:27:35 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@760 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/ReversiStrategyFaMa.java
@@ -8,11 +8,24 @@ public class ReversiStrategyFaMa implements ReversiStrategy
 
     public Piece move(GameModel gameModel)
     {
-        return null;
+        int best = Integer.MIN_VALUE;
+        Piece bestMove = null;
+        for (int i = 0; i < GameModel.SIZE; i++)
+        {
+            for (int j = 0; j < GameModel.SIZE; j++)
+            {
+                if (gameModel.getLegalMoves(gameModel.getTurnHolder())[i][j])
+                {
+                    GameModel clone = new GameModel(gameModel); //erzeugt ein Klon des aktuellen Spielstands
+                    int value = mm
+                }
+            }
+        }   
     }
 
     public int getCount()
     {
-        return 0;
+        return 5;
     }
+
 }
