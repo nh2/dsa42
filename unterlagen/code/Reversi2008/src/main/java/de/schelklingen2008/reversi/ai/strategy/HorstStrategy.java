9ccd743a8024ed97104c9070936d6f499ce186e4
niklash <niklash@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 16:51:48 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@806 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/HorstStrategy.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/HorstStrategy.java
@@ -2,6 +2,7 @@ package de.schelklingen2008.reversi.ai.strategy;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import de.schelklingen2008.reversi.ai.evaluation.EvaluationFunction;
 import de.schelklingen2008.reversi.ai.evaluation.HorstEvaluation;
@@ -12,7 +13,7 @@ import de.schelklingen2008.reversi.model.Player;
 public class HorstStrategy implements ReversiStrategy
 {
 
-    EvaluationFunction evalfunc    = new HorstEvaluation();
+    EvaluationFunction evalfunc;
     private int        searchDepth = 3;
 
     private Player     max;
@@ -29,21 +30,31 @@ public class HorstStrategy implements ReversiStrategy
 
     public Piece move(GameModel gameModel)
     {
+        if (evalfunc == null) evalfunc = new HorstEvaluation(gameModel);
         return minimax(gameModel);
     }
+
+    private List<GameModel> cloneList  = new ArrayList<GameModel>(10000);
+    private List<GameModel> cloneList2 = new ArrayList<GameModel>(10000);
+    int                     clones     = 0;
+    int                     clones2    = 0;
 
     private Piece minimax(GameModel game)
     {
         max = game.getTurnHolder();
         int best = Integer.MIN_VALUE;
         Piece bestMove = null;
-        List<Piece> legalMoves = getLegalMoves(game, max);
 
+        Set<Piece> legalMoves = game.getLegalMovesSet(max);
         for (Piece move : legalMoves)
         {
             GameModel clone = new GameModel(game);
+            // cloneList.add(new GameModel(game));
+            // GameModel clone = cloneList.get(clones);
+            // clones++;
             clone.placePiece(move.getX(), move.getY(), max);
             int value = minimaxval(clone, searchDepth);
+            // clones--;
             if (value > best)
             {
                 best = value;
@@ -56,20 +67,23 @@ public class HorstStrategy implements ReversiStrategy
 
     private int minimaxval(GameModel game, int depth)
     {
-        if (depth == 0) return evalfunc.evaluatePosition(game, game.getTurnHolder());
+        if (depth == 0) return evalfunc.evaluatePosition(game, max);
         int best = 0;
         if (game.getTurnHolder() == max)
             best = Integer.MIN_VALUE;
         else
             best = Integer.MAX_VALUE;
 
-        List<Piece> legalMoves = getLegalMoves(game, max);
-
+        Set<Piece> legalMoves = game.getLegalMovesSet(game.getTurnHolder());
         for (Piece move : legalMoves)
         {
             GameModel clone = new GameModel(game);
+            // cloneList2.add(new GameModel(game));
+            // GameModel clone = cloneList2.get(clones2);
+            // clones2++;
             clone.placePiece(move.getX(), move.getY(), game.getTurnHolder());
-            int val = minimaxval(clone, searchDepth);
+            int val = minimaxval(clone, depth - 1);
+            // clones2--;
             if (game.getTurnHolder() == max)
             {
                 if (val > best) best = val;
@@ -83,16 +97,17 @@ public class HorstStrategy implements ReversiStrategy
         return best;
     }
 
-    private List<Piece> getLegalMoves(GameModel game, Player turnHolder)
-    {
-        List<Piece> legalMoves = new ArrayList<Piece>();
-        for (int i = 0; i < game.getLegalMoves(turnHolder).length; i++)
-        {
-            for (int k = 0; k < game.getLegalMoves(turnHolder)[i].length; k++)
-            {
-                if (game.getLegalMoves(turnHolder)[i][k]) legalMoves.add(new Piece(i, k, game.getTurnHolder()));
-            }
-        }
-        return legalMoves;
-    }
+    // private List<Piece> getLegalMoves(GameModel game, Player turnHolder)
+    // {
+    // List<Piece> legalMoves = new ArrayList<Piece>();
+    // boolean[][] legalMoves2 = game.getLegalMoves(turnHolder);
+    // for (int i = 0; i < legalMoves2.length; i++)
+    // {
+    // for (int k = 0; k < legalMoves2[i].length; k++)
+    // {
+    // if (legalMoves2[i][k]) legalMoves.add(new Piece(i, k, game.getTurnHolder()));
+    // }
+    // }
+    // return legalMoves;
+    // }
 }
