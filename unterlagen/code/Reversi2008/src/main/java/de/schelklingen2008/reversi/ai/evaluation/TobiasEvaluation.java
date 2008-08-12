42a60f2fccd73cebb03afbbac770182136db3285
tobias <tobias@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 09:52:35 +0000

    Tobias Strategy + Eval-Dummy committed
    git-svn-id: http://deditus.de/svn/dsarepo42@769 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/TobiasEvaluation.java
@@ -0,0 +1,12 @@
+package de.schelklingen2008.reversi.ai.evaluation;
+
+import de.schelklingen2008.reversi.model.GameModel;
+
+public class TobiasEvaluation
+{
+
+    public static int eval(GameModel model)
+    {
+        return 0;
+    }
+}
---
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/TobiasStrategy.java
@@ -0,0 +1,105 @@
+package de.schelklingen2008.reversi.ai.strategy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.schelklingen2008.reversi.ai.evaluation.TobiasEvaluation;
+import de.schelklingen2008.reversi.model.GameModel;
+import de.schelklingen2008.reversi.model.Piece;
+import de.schelklingen2008.reversi.model.Player;
+
+public class TobiasStrategy
+{
+
+    public Piece minimax(GameModel model, int depth)
+    {
+        int best = Integer.MIN_VALUE;
+        int value;
+        Piece bestMove = null;
+        Player me = model.getTurnHolder();
+        boolean[][] legalMovesArray = model.getLegalMoves(me);
+        List<Piece> legalMovesList = new ArrayList<Piece>();
+
+        for (int i = 0; i < legalMovesArray.length; i++)
+        {
+            for (int j = 0; j < legalMovesArray[i].length; j++)
+            {
+                if (legalMovesArray[i][j])
+                {
+                    legalMovesList.add(new Piece(i, j, me));
+                }
+            }
+        }
+
+        for (int i = 0; i < legalMovesList.size(); i++)
+        {
+            GameModel clone = new GameModel(model);
+            Piece piece = legalMovesList.get(i);
+            clone.placePiece(piece.getX(), piece.getY(), piece.getPlayer());
+            value = minimaxvalue(model, depth, me);
+            if (value > best)
+            {
+                best = value;
+                bestMove = piece;
+            }
+        }
+        return bestMove;
+    }
+
+    private int minimaxvalue(GameModel model, int depth, Player me)
+    {
+        if (depth == 0)
+        {
+            return TobiasEvaluation.eval(model);
+        }
+
+        int best;
+        Player turnHolder = model.getTurnHolder();
+        boolean[][] legalMovesArray = model.getLegalMoves(turnHolder);
+        List<Piece> legalMovesList = new ArrayList<Piece>();
+        int val;
+
+        if (me == turnHolder)
+        {
+            best = Integer.MIN_VALUE;
+        }
+        else
+        {
+            best = Integer.MAX_VALUE;
+        }
+
+        for (int i = 0; i < legalMovesArray.length; i++)
+        {
+            for (int j = 0; j < legalMovesArray[i].length; j++)
+            {
+                if (legalMovesArray[i][j])
+                {
+                    legalMovesList.add(new Piece(i, j, turnHolder));
+                }
+            }
+        }
+
+        for (int i = 0; i < legalMovesList.size(); i++)
+        {
+            GameModel clone = new GameModel(model);
+            Piece piece = legalMovesList.get(i);
+            clone.placePiece(piece.getX(), piece.getY(), piece.getPlayer());
+            val = minimaxvalue(clone, depth - 1, me);
+            if (me == turnHolder)
+            {
+                if (val < best)
+                {
+                    best = val;
+                }
+            }
+            else
+            {
+                if (val > best)
+                {
+                    best = val;
+                }
+            }
+        }
+        return best;
+    }
+}
