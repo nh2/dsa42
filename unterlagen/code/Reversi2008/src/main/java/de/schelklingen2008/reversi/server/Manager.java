93d336565cc3da110643eb1f60ca812f36a0662c
ben <ben@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 08:46:27 +0000

    log a warning in case of exception
    git-svn-id: http://deditus.de/svn/dsarepo42@757 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/server/Manager.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/server/Manager.java
@@ -1,5 +1,7 @@
 package de.schelklingen2008.reversi.server;
 
+import java.util.logging.Logger;
+
 import com.threerings.crowd.data.BodyObject;
 import com.threerings.crowd.data.PlaceObject;
 import com.threerings.parlor.game.server.GameManager;
@@ -7,6 +9,7 @@ import com.threerings.parlor.game.server.GameManager;
 import de.schelklingen2008.reversi.model.GameModel;
 import de.schelklingen2008.reversi.model.Player;
 import de.schelklingen2008.reversi.transport.SharedState;
+import de.schelklingen2008.util.LoggerFactory;
 
 /**
  * Handles the server side of the game.
@@ -14,11 +17,13 @@ import de.schelklingen2008.reversi.transport.SharedState;
 public class Manager extends GameManager
 {
 
+    private static final Logger sLogger = LoggerFactory.create();
+
     /** Is the state transmitted to the clients and managed by the server. */
-    private SharedState sharedState;
+    private SharedState         sharedState;
 
     /** Implements the game logic with an own internal model. */
-    private GameModel   gameModel;
+    private GameModel           gameModel;
 
     @Override
     protected PlaceObject createPlaceObject()
@@ -40,7 +45,22 @@ public class Manager extends GameManager
      */
     public void placePiece(BodyObject client, int x, int y)
     {
-        gameModel.placePiece(x, y, getPlayer(client));
+        try
+        {
+            gameModel.placePiece(x, y, getPlayer(client));
+        }
+        catch (Exception e)
+        {
+            String msg = "cannot place piece: x: "
+                         + x
+                         + " y: "
+                         + y
+                         + " player: "
+                         + client.username
+                         + " reason: "
+                         + e.getMessage();
+            sLogger.warning(msg);
+        }
         updateSharedState();
         if (gameModel.isFinished()) endGame();
     }
