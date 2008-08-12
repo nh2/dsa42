91bf970839d450ecb7fc58e1aaafca79c8805761
alexander <alexander@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-12 09:38:28 +0000

    implemented mmVal() and minimax()
    git-svn-id: http://deditus.de/svn/dsarepo42@763 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/AmorStrategy.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/strategy/AmorStrategy.java
@@ -1,10 +1,17 @@
 package de.schelklingen2008.reversi.ai.strategy;
 
+import org.apache.tools.ant.taskdefs.Move;
+
+import com.threerings.ezgame.Game;
+
 import de.schelklingen2008.reversi.model.GameModel;
 import de.schelklingen2008.reversi.model.Piece;
+import de.schelklingen2008.reversi.model.Player;
 
 public class AmorStrategy implements ReversiStrategy
 {
+
+    Player player;
 
     public int getCount()
     {
@@ -14,8 +21,65 @@ public class AmorStrategy implements ReversiStrategy
 
     public Piece move(GameModel gameModel)
     {
+        player = gameModel.getTurnHolder();
 
         return null;
     }
 
+    public Move minimax(GameModel gameModel)
+    {
+        int best = Integer.MAX_VALUE;
+        Move bestMove = null;
+        int value;
+        Player hilf = gameModel.getTurnHolder();
+        for (int i = 0; i < gameModel.getLegalMoves(hilf).length; i++)
+        {
+
+            for (int j = 0; j < gameModel.getLegalMoves(hilf)[0].length; j++)
+            {
+                Game clone = game.copy();
+                value = mmVal(clone.move(move), 5);
+                if (value > best)
+                {
+                    best = value;
+                    bestMove = move;
+
+                }
+            }
+
+            return bestMove;
+
+        }
+
+    }
+
+    public int mmVal(GameModel gameModel, int depth)
+    {
+        if (depth == 0) return eval(gameModel);
+        int best;
+        int value;
+        Player hilf = gameModel.getTurnHolder();
+        if (gameModel.isTurnHolder(player))
+            best = Integer.MIN_VALUE;
+        else
+            best = Integer.MAX_VALUE;
+
+        for (int i = 0; i < gameModel.getLegalMoves(hilf).length; i++)
+        {
+
+            for (int j = 0; j < gameModel.getLegalMoves(hilf)[0].length; j++)
+            {
+                Game clone = game.copy();
+                value = mmVal(clone.move(move), depth - 1);
+            }
+        }
+        if (gameModel.isTurnHolder(player))
+        {
+            if (value > best)
+                best = value;
+            else if (value < best) best = value;
+        }
+        return best;
+
+    }
 }
