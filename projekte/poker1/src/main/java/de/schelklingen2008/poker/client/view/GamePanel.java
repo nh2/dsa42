c7175cad87f680cf6e48d84760a521179f6d1006
tobias <tobias@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-14 16:51:25 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@1026 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/poker1/src/main/java/de/schelklingen2008/poker/client/view/GamePanel.java
+++ projekte/poker1/src/main/java/de/schelklingen2008/poker/client/view/GamePanel.java
@@ -66,16 +66,13 @@ public class GamePanel extends JPanel implements PlaceView
         vlabel.setFont(ToyBoxUI.fancyFont);
         sidePanel.add(vlabel, GroupLayout.FIXED);
 
-        // HistoryPanel historyPanel = new HistoryPanel(controller);
-
         // add a chat box
         sidePanel.add(new ChatPanel(controller.getToyBoxContext()));
-        // sidePanel.add(historyPanel);
 
-        // add a standard turn display
-        TurnPanel turnDisplay = new TurnPanel(controller);
-        turnDisplay.setOpaque(false);
-        sidePanel.add(turnDisplay, GroupLayout.FIXED);
+        // add a history panel
+        HistoryPanel historyDisplay = new HistoryPanel(controller);
+        historyDisplay.setOpaque(false);
+        sidePanel.add(historyDisplay, GroupLayout.FIXED);
 
         // add a "back to lobby" button
         JButton back = new JButton();
--- projekte/poker1/src/main/java/de/schelklingen2008/poker/client/view/HistoryPanel.java
+++ projekte/poker1/src/main/java/de/schelklingen2008/poker/client/view/HistoryPanel.java
@@ -1,12 +1,16 @@
 package de.schelklingen2008.poker.client.view;
 
+import java.awt.Color;
 import java.util.List;
 
+import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
+import de.schelklingen2008.poker.client.Constants;
 import de.schelklingen2008.poker.client.controller.Controller;
 import de.schelklingen2008.poker.client.controller.GameChangeListener;
+import de.schelklingen2008.poker.model.GameModel;
 
 public class HistoryPanel extends JPanel implements GameChangeListener
 {
@@ -16,26 +20,35 @@ public class HistoryPanel extends JPanel implements GameChangeListener
     public HistoryPanel(Controller controller)
     {
         this.controller = controller;
+        controller.addChangeListener(this);
         paintHistoryPanel();
-    }
-
-    public void labels()
-    {
-        add(new JLabel("Text..."));
     }
 
     public void gameChanged()
     {
-        removeAll();
         paintHistoryPanel();
     }
 
     private void paintHistoryPanel()
     {
-        List<String> historyList = controller.getGameContext().getGameModel().getHistoryList();
+        removeAll();
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        GameModel model = controller.getGameContext().getGameModel();
+        if (model == null) return;
+        List<String> historyList = model.getHistoryList();
+
         for (String s : historyList)
         {
-            add(new JLabel(s));
+            add(createLabel(s));
         }
+    }
+
+    private JLabel createLabel(String text)
+    {
+        JLabel label = new JLabel(text);
+        label.setOpaque(false);
+        label.setForeground(Color.WHITE);
+        label.setBackground(Constants.BACK_GREEN);
+        return label;
     }
 }
