2adb9c9d85294a756cbdcc6c42e72166ef5f5ff8
fabio <fabio@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-14 16:31:17 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@1012 0c20435e-6aac-11dd-8100-c6319ae65bfc

---
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/ChaosEvaluation.java
@@ -0,0 +1,48 @@
+package de.schelklingen2008.reversi.ai.evaluation;
+
+import de.schelklingen2008.reversi.model.GameModel;
+import de.schelklingen2008.reversi.model.Player;
+
+public class ChaosEvaluation implements EvaluationFunction
+{
+
+    public int evaluatePosition(GameModel gameModel, Player player)
+    {
+        int mReturn = 0;
+        if (gameModel.isFinished() && gameModel.isWinner(player)) return 10000;
+        if (gameModel.isFinished() && !gameModel.isWinner(player)) return -10000;
+
+        mReturn += gameModel.countPieces(player) * 2;
+        if (gameModel.countFree() <= 5)
+        {
+            mReturn += Math.pow(gameModel.getBoard().length, 4) + Math.pow(gameModel.getBoard().length, 3);
+        }
+
+        // rank possible moves
+        if (gameModel.getLegalMovesSet(player.other()).size() == 0)
+        {
+            mReturn += 150;
+        }
+
+        // rank fields
+        int[][] fieldRanks = new int[][] { { 1000, -500, 200, 100, 100, 200, -500, 1000 },
+                { -500, -250, 200, 100, 100, 200, -500, -500 }, { 300, -125, 200, 100, 100, 200, -500, 300 },
+                { 200, -60, 200, 100, 100, 200, -500, 200 }, { 200, -60, 200, 100, 100, 200, -500, 200 },
+                { 300, -125, 200, 100, 100, 200, -500, 300 }, { -500, -250, 200, 100, 100, 200, -500, -500 },
+                { 1000, -500, 300, 200, 200, 200, -500, 1000 } };
+        Player[][] gameBoard = gameModel.getBoard();
+
+        for (int v = 0; v < GameModel.SIZE; v++)// vertical
+        {
+            for (int h = 0; h < GameModel.SIZE; h++)// horizontal
+            {
+                if (gameBoard[v][h] == player)
+                {
+                    mReturn += fieldRanks[v][h];
+                }
+            }
+        }
+
+        return mReturn;
+    }
+}
