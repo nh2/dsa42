2072c7676a33dd9789bbe4e47c2304f8d62dfd3a
alexander <alexander@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-14 16:14:40 +0000

    fixed bug
    git-svn-id: http://deditus.de/svn/dsarepo42@991 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- projekte/Canasta/src/main/java/de/schelklingen2008/canasta/client/view/TurnPanel.java
+++ projekte/Canasta/src/main/java/de/schelklingen2008/canasta/client/view/TurnPanel.java
@@ -1,8 +1,14 @@
 package de.schelklingen2008.canasta.client.view;
 
+import java.awt.Color;
+import java.awt.Polygon;
+import java.awt.geom.Ellipse2D;
+
 import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import com.samskivert.swing.ShapeIcon;
 
 import de.schelklingen2008.canasta.client.controller.Controller;
 import de.schelklingen2008.canasta.client.controller.GameChangeListener;
@@ -16,7 +22,10 @@ import de.schelklingen2008.canasta.model.Player;
 public class TurnPanel extends JPanel implements GameChangeListener
 {
 
-    private Controller controller;
+    private static final Polygon   TRIANGLE  = new Polygon(new int[] { 0, 12, 0 }, new int[] { 0, 6, 12 }, 3);
+    private static final Ellipse2D CIRCLE    = new Ellipse2D.Float(0, 0, 12, 12);
+    private static final ShapeIcon ICON_TURN = new ShapeIcon(TRIANGLE, Color.YELLOW, null);
+    private Controller             controller;
 
     public TurnPanel(Controller controller)
     {
@@ -35,7 +44,9 @@ public class TurnPanel extends JPanel implements GameChangeListener
         for (Player player : getGameModel().getPlayers())
         {
             String name = player.getName();
-            add(new JLabel(name + "       " + player.getTotalScore()));
+            JLabel label = new JLabel(name + "       " + player.getTotalScore());
+            if (controller.getGameContext().getGameModel().getTurnHolder() == 0) label.setIcon(ICON_TURN);
+            add(label);
 
         }
 
