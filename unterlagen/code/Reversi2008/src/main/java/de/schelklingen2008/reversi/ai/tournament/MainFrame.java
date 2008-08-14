5027125536c9826d1acb06fba66c64bb442432eb
leonhard <leonhard@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-14 21:49:42 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@1036 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/tournament/MainFrame.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/tournament/MainFrame.java
@@ -26,7 +26,7 @@ public class MainFrame
 
     public static void main(String[] args)
     {
-        Tournament t = new Tournament(4);
+        Tournament t = new Tournament(6);
         t.addStrategy(new TournamentStrategy("GeBen1", new AlphaBetaStrategy(new GeorgEvaluationFunction(),
                                                                              1,
                                                                              1,
