5e231aa7f73ff41b524b6ae2f4a933b04bf7d88f
georg <georg@0c20435e-6aac-11dd-8100-c6319ae65bfc>
2008-08-14 15:56:52 +0000

    git-svn-id: http://deditus.de/svn/dsarepo42@975 0c20435e-6aac-11dd-8100-c6319ae65bfc

--- unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/EvaluationFunction.java
+++ unterlagen/code/Reversi2008/src/main/java/de/schelklingen2008/reversi/ai/evaluation/EvaluationFunction.java
@@ -1,12 +1,11 @@
 package de.schelklingen2008.reversi.ai.evaluation;
 
 import de.schelklingen2008.reversi.model.GameModel;
-import de.schelklingen2008.reversi.model.Piece;
 import de.schelklingen2008.reversi.model.Player;
 
 public interface EvaluationFunction
 {
 
-    int evaluatePosition(GameModel gameModel, Player player, Piece move);
+    int evaluatePosition(GameModel gameModel, Player player);
 
 }
